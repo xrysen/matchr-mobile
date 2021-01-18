42434ed0c5bd6eb9f81d771ea998134125d0b1d5
Sean Oyler <xrysen@gmail.com>
2021-01-18 14:06:24 -0800

    Adds textured background and changes colour

--- Components/FriendList.jsx
+++ Components/FriendList.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, ImageBackground } from "react-native";
 import { useFonts } from "expo-font";
 import Loading from "./Loading";
 import GradientButton from "./GradientButton";
 import { TouchableOpacity } from "react-native-gesture-handler";
+
+const bgImage = { uri: "https://www.transparenttextures.com/patterns/black-paper.png" };
 
 const FriendList = ( {navigation } ) => {
   let [fontsLoaded] = useFonts({
@@ -17,20 +19,23 @@ const FriendList = ( {navigation } ) => {
   return (
     <View style={styles.container}>
       <View style={styles.profile}>
+        <ImageBackground source={bgImage} style={{width: "100%", alignItems: "center"}}>
+
         <Image
           source={require("../assets/Avatars/avatar1.png")}
           style={{ marginTop: 20, marginBottom: 20, borderRadius: 10 }}
-        />
+          />
         <Text
           style={{ color: "white", fontFamily: "Lora-Medium", fontSize: 20 }}
-        >
+          >
           Welcome, Sally!
         </Text>
         <Text
           style={{ color: "white", fontSize: 20, fontFamily: "Lora-Medium" }}
-        >
+          >
           Who are we dining with?
         </Text>
+          </ImageBackground>
       </View>
       <View style={styles.friendsList}></View>
       <View style={{ width: "100%", flexDirection: "row", marginTop: 30 }}>
@@ -89,15 +94,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#846C9C",
     width: "100%",
     alignItems: "center",
-    borderBottomRightRadius: 80,
-    borderBottomLeftRadius: 80,
   },
   container: {
     marginTop: 30,
     height: "100%",
   },
   friendsList: {
-    backgroundColor: "white",
+    backgroundColor: "black",
     position: "absolute",
     width: "100%",
     height: "100%",
